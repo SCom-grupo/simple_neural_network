e6aa64173e10bfa810c1e759788f4aa994e76afd
André Nóbrega <andrenobrega2000@gmail.com>
2023-05-23 17:25:05 +0100

    Added code to create weight files, encrypt now reads weights from files and performs linearwsum

---
+++ encryptTeste.cpp
@@ -0,0 +1,305 @@
+/* This code reads the weights from the weights files and the encrypted data and does the calculations*/
+/* This specific version also outputs a ExpectedResults file to be compared with the results file to verify correct operation*/
+#include "openfhe.h"
+
+// header files needed for serialization
+#include "ciphertext-ser.h"
+#include "cryptocontext-ser.h"
+#include "key/key-ser.h"
+#include "scheme/ckksrns/ckksrns-ser.h"
+
+#define PROFILE
+using namespace lbcrypto;
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <random>
+#include <algorithm>
+
+using namespace std;
+
+const std::string DATAFOLDER = "Data";
+
+vector<string> split(const string &s, char delim) {
+    stringstream ss(s);
+    string item;
+    vector<string> tokens;
+    while (getline(ss, item, delim)) {
+        tokens.push_back(item);
+    }
+    return tokens;
+}
+
+int main(int argc, const char * argv[]) {
+    uint32_t multDepth = 1;
+    uint32_t scaleModSize = 50;
+    uint32_t batchSize = 1<<13;
+
+    CCParams<CryptoContextCKKSRNS> parameters;
+    parameters.SetMultiplicativeDepth(multDepth);
+    parameters.SetScalingModSize(scaleModSize);
+    parameters.SetBatchSize(batchSize);
+
+    CryptoContext<DCRTPoly> cc = GenCryptoContext(parameters);
+
+    // Enable the features that you wish to use
+    cc->Enable(PKE);
+    cc->Enable(KEYSWITCH);
+    cc->Enable(LEVELEDSHE);
+    cc->Enable(ADVANCEDSHE);
+    std::cout << "CKKS scheme is using ring dimension " << cc->GetRingDimension() << std::endl << std::endl;
+
+    std::cout << "\nThe cryptocontext has been generated." << std::endl;
+
+    // Serialize cryptocontext
+    if (!Serial::SerializeToFile(DATAFOLDER + "/cryptocontext.txt", cc, SerType::BINARY)) {
+        std::cerr << "Error writing serialization of the crypto context to "
+                     "cryptocontext.txt"
+                  << std::endl;
+        return 1;
+    }
+    std::cout << "The cryptocontext has been serialized." << std::endl;
+
+    auto keys = cc->KeyGen();
+
+    std::cout << "The key pair has been generated." << std::endl;
+
+    // Serialize the public key
+    if (!Serial::SerializeToFile(DATAFOLDER + "/key-public.txt", keys.publicKey, SerType::BINARY)) {
+        std::cerr << "Error writing serialization of public key to key-public.txt" << std::endl;
+        return 1;
+    }
+    std::cout << "The public key has been serialized." << std::endl;
+
+    // Serialize the secret key
+    if (!Serial::SerializeToFile(DATAFOLDER + "/key-private.txt", keys.secretKey, SerType::BINARY)) {
+        std::cerr << "Error writing serialization of private key to key-private.txt" << std::endl;
+        return 1;
+    }
+    std::cout << "The secret key has been serialized." << std::endl;
+
+    cc->EvalMultKeyGen(keys.secretKey);
+
+    string line;
+    vector<string> line_v;
+
+    cout << "Loading data ...\n";
+    ifstream input (DATAFOLDER + "/test.txt");
+    ifstream inputW1 (DATAFOLDER + "/w1.txt");
+    ifstream inputW2 (DATAFOLDER + "/w2.txt");
+    ifstream inputW3 (DATAFOLDER + "/w3.txt");
+    ofstream output1 (DATAFOLDER + "/Data.txt");
+    ofstream output2 (DATAFOLDER + "/Label.txt");
+    ofstream output3 (DATAFOLDER + "/DataRec.txt");
+    ofstream output4 (DATAFOLDER + "/Result.txt");
+    ofstream output5 (DATAFOLDER + "/ExpectedResult.txt");
+
+    // Initializing the vector of vectors
+    vector<vector<double>> X_train;
+    vector<vector<double>> y_train;
+    vector<vector<double>> X_train_T;
+    vector<vector<double>> y_train_T;
+
+    std::vector<std::vector<double>> W1;
+    std::vector<std::vector<double>> W2;
+    std::vector<std::vector<double>> W3;
+
+    float calc;
+    
+    // Reads the data and separates in labels (y_train) and bits (X_train)
+    if (input.is_open())
+    {
+        while ( getline (input,line) )
+        {
+            line_v = split(line, '\t');
+
+            vector<double>  tempy;
+            unsigned digit = strtof((line_v[0]).c_str(),0);
+            for (unsigned i = 0; i < 10; ++i) {
+                if (i == digit)
+                {
+                    tempy.push_back(1.);
+                }
+                else tempy.push_back(0.);
+            }
+            
+            vector<double>  tempx;
+            unsigned size = static_cast<int>(line_v.size());
+            for (unsigned i = 1; i < size; ++i) {
+                calc=strtof((line_v[i]).c_str(),0);
+                tempx.push_back(calc/255.0);
+            }
+
+            y_train.push_back(tempy);
+            X_train.push_back(tempx);
+        }
+        input.close();
+
+        // Reads the weight file W1
+        while ( getline (inputW1,line) )
+        {
+            line_v = split(line, '\t');
+            
+            vector<double>  tempw;
+            unsigned size = static_cast<int>(line_v.size());
+            for (unsigned i = 0; i < size; ++i) {
+                tempw.push_back(strtof((line_v[i]).c_str(),0)); 
+            }
+            W1.push_back(tempw);
+        }
+        inputW1.close();
+
+        // Reads the weight file W2
+        while ( getline (inputW2,line) )
+        {
+            line_v = split(line, '\t');
+            
+            vector<double>  tempw;
+            unsigned size = static_cast<int>(line_v.size());
+            for (unsigned i = 0; i < size; ++i) {
+                tempw.push_back(strtof((line_v[i]).c_str(),0));
+            }
+            W2.push_back(tempw);
+        }
+        inputW2.close();
+
+        // Reads the weight file W3
+        while ( getline (inputW3,line) )
+        {
+            line_v = split(line, '\t');
+            
+            vector<double>  tempw;
+            unsigned size = static_cast<int>(line_v.size());
+            for (unsigned i = 0; i < size; ++i) {
+                tempw.push_back(strtof((line_v[i]).c_str(),0));
+            }
+            W3.push_back(tempw);
+        }
+        inputW3.close();
+        
+        // Transposes the bits matrix (X_train) so that in can be encrypted in columns
+        // Also outputs the transposed matrix to output1 for checking the correct operation
+        unsigned size1 = static_cast<int>(X_train.size());
+        unsigned size2 = static_cast<int>(X_train[0].size());
+        for (unsigned i2 = 0; i2 < size2; ++i2) {
+            vector<double>  tempx_T;
+            for (unsigned i1 = 0; i1 < size1; ++i1) {
+                output1 << X_train[i1][i2] << "\t";
+                tempx_T.push_back(X_train[i1][i2]);
+            }
+            output1.seekp(-1 ,ios::cur);
+            output1 << "\n";
+            X_train_T.push_back(tempx_T);
+        }
+
+        // Transposes the bits matrix (y_train) so that in can be encrypted in columns
+        // Also outputs the transposed matrix to output2 for checking the correct operation
+        unsigned size3 = static_cast<int>(y_train.size());
+        unsigned size4 = static_cast<int>(y_train[0].size());
+        for (unsigned i2 = 0; i2 < size4; ++i2) {
+            vector<double>  tempy_T;
+            for (unsigned i1 = 0; i1 < size3; ++i1) {
+                output2 << y_train[i1][i2] << "\t";
+                tempy_T.push_back(y_train[i1][i2]);
+            }
+            output2.seekp(-1 ,ios::cur);
+            output2 << "\n";
+            y_train_T.push_back(tempy_T);
+        }
+
+        // Encrypts the transposed matrix (X_train_T) in lines to ciphertextVecx
+        std::vector<ConstCiphertext<DCRTPoly>> ciphertextVecx;
+        for (unsigned i = 0; i < size2; ++i) {
+            Plaintext plaintext = cc->MakeCKKSPackedPlaintext(X_train_T[i]);
+            ciphertextVecx.push_back(cc->Encrypt(keys.publicKey, plaintext));
+        }
+
+        // Outputs the encrypted data to file /Encrypteddata.txt
+        if (!Serial::SerializeToFile(DATAFOLDER + "/Encrypteddata.txt", ciphertextVecx, SerType::BINARY)) {
+            std::cerr << "Error writing serialization of the encrypteddata to "
+                     "Encrypteddata.txt"
+            << std::endl;
+            return 1;
+        }
+        std::cout << "The encrypteddata has been serialized." << std::endl;
+
+        // Encrypts the transposed matrix (y_train_T) in lines to ciphertextVecy
+        std::vector<ConstCiphertext<DCRTPoly>> ciphertextVecy;
+        for (unsigned i = 0; i < size4; ++i) {
+            Plaintext plaintext = cc->MakeCKKSPackedPlaintext(y_train_T[i]);
+            ciphertextVecy.push_back(cc->Encrypt(keys.publicKey, plaintext));
+        }
+
+        // Outputs the encrypted labels to file /Encryptedlabel.txt
+        if (!Serial::SerializeToFile(DATAFOLDER + "/Encryptedlabel.txt", ciphertextVecy, SerType::BINARY)) {
+            std::cerr << "Error writing serialization of the encryptedlabel to "
+                     "Encryptedlabel.txt"
+            << std::endl;
+            return 1;
+        }
+        std::cout << "The encryptedlabel has been serialized." << std::endl;
+
+        // Clears cryptocontext to check correct functioning
+        cc->ClearEvalMultKeys();
+        lbcrypto::CryptoContextFactory<lbcrypto::DCRTPoly>::ReleaseAllContexts();
+
+        // Read the crypto context from file /cryptocontext.txt
+        CryptoContext<DCRTPoly> cc;
+        if (!Serial::DeserializeFromFile(DATAFOLDER + "/cryptocontext.txt", cc, SerType::BINARY)) {
+            std::cerr << "I cannot read serialization from " << "cryptocontext.txt" << std::endl;
+            return 1;
+        }
+        std::cout << "The cryptocontext has been deserialized." << std::endl;
+
+        // Reads public key from file /key-public.txt
+        PublicKey<DCRTPoly> pk;
+        if (Serial::DeserializeFromFile(DATAFOLDER + "/key-public.txt", pk, SerType::BINARY) == false) {
+            std::cerr << "Could not read public key" << std::endl;
+            return 1;
+        }
+        std::cout << "The public key has been deserialized." << std::endl;
+
+        // Reads private key from file /key-private.txt
+        PrivateKey<DCRTPoly> sk;
+        if (Serial::DeserializeFromFile(DATAFOLDER + "/key-private.txt", sk, SerType::BINARY) == false) {
+            std::cerr << "Could not read secret key" << std::endl;
+            return 1;
+        }
+        std::cout << "The secret key has been deserialized." << std::endl;
+
+        // Reads encrypted data from file /Encrypteddata.txt
+        std::vector<ConstCiphertext<DCRTPoly>> ciphertextVecx_R;
+        if (Serial::DeserializeFromFile(DATAFOLDER + "/Encrypteddata.txt", ciphertextVecx_R, SerType::BINARY) == false) {
+            std::cerr << "Could not read the ciphertext" << std::endl;
+            return 1;
+        }
+        std::cout << "The first ciphertext has been deserialized." << std::endl;
+
+        // Performs the LinearWSum of ciphertextVecx_R with W1 and outputs the results to output4
+        for(unsigned i = 0; i < 128; ++i){
+            auto result = cc->EvalLinearWSum(ciphertextVecx_R, W1[i]);
+            Plaintext resultDecrypted;
+            cc->Decrypt(sk, result, &resultDecrypted);
+            output4 << resultDecrypted;
+            std::cout << "Calculated and decrypted line i=" << i << std::endl;
+        }
+
+        // Does the same operation but with unencrypted data to check correct operation to output5
+        for(unsigned i1 = 0; i1 < 128; ++i1){
+            for (unsigned i2 = 0; i2 < 1000; ++i2) {
+                double x=0;
+                for (usint j = 0; j < 784; ++j) {
+                    x += X_train_T[j][i2] * W1[i1][j];
+                }
+                output5 << x << "\t";
+            }
+            output5 << "\n";
+        }
+
+    }
+    else cout << "Unable to open file" << '\n';
+}
---
+++ nn.cpp
@@ -0,0 +1,471 @@
+//
+//  nn.cpp
+//  
+//  To compile: g++ -o nn nn.cpp -std=c++11
+//  To run: ./nn
+//  Created by Sergei Bugrov on 4/20/18.
+//  Copyright © 2017 Sergei Bugrov. All rights reserved.
+//  Download dataset from: https://drive.google.com/file/d/1OdtwXHf_-2T0aS9HLBnxU3o-72mklCZY/view?usp=sharing
+//
+/* This code was modified to generate the 3 weight files */
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <random>
+#include <algorithm>
+
+using namespace std;
+
+const std::string DATAFOLDER = "Data";
+
+vector <float> operator/(const vector <float>& m2, const float m1);
+
+void print ( const vector <float>& m, int n_rows, int n_columns ) {
+    
+    /*  "Couts" the input vector as n_rows x n_columns matrix.
+     Inputs:
+     m: vector, matrix of size n_rows x n_columns
+     n_rows: int, number of rows in the left matrix m1
+     n_columns: int, number of columns in the left matrix m1
+     */
+    
+    for( int i = 0; i != n_rows; ++i ) {
+        for( int j = 0; j != n_columns; ++j ) {
+            cout << m[ i * n_columns + j ] << " ";
+        }
+        cout << '\n';
+    }
+    cout << endl;
+}
+
+int argmax ( const vector <float>& m ) {
+
+    return distance(m.begin(), max_element(m.begin(), m.end()));
+}
+
+vector <float> relu(const vector <float>& z){
+    int size = z.size();
+    vector <float> output;
+    for( int i = 0; i < size; ++i ) {
+        if (z[i] < 0){
+            output.push_back(0.0);
+        }
+        else output.push_back(z[i]);
+    }
+    return output;
+}
+
+vector <float> reluPrime (const vector <float>& z) {
+    int size = z.size();
+    vector <float> output;
+    for( int i = 0; i < size; ++i ) {
+        if (z[i] <= 0){
+            output.push_back(0.0);
+        }
+        else output.push_back(1.0);
+    }
+    return output;
+} 
+
+static vector<float> random_vector(const int size)
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> distribution(0.0, 0.05);
+    static default_random_engine generator;
+
+    vector<float> data(size);
+    generate(data.begin(), data.end(), [&]() { return distribution(generator); });
+    return data;
+}
+
+vector <float> softmax (const vector <float>& z, const unsigned dim) {
+    
+    const unsigned zsize = static_cast<int>(z.size());
+    vector <float> out;
+    
+    for (unsigned i = 0; i != zsize; i += dim) {
+        vector <float> foo;
+        for (unsigned j = 0; j != dim; ++j) {
+            foo.push_back(z[i + j]);
+        }
+        
+        float max_foo = *max_element(foo.begin(), foo.end());
+
+        for (unsigned j = 0; j != dim; ++j) {
+            foo[j] = exp(foo[j] - max_foo);
+        }      
+
+        float sum_of_elems = 0.0;
+        for (unsigned j = 0; j != dim; ++j) {
+            sum_of_elems = sum_of_elems + foo[j];
+        }
+        
+        for (unsigned j = 0; j != dim; ++j) {
+            out.push_back(foo[j]/sum_of_elems);
+        }
+    }
+    return out;
+}
+
+vector <float> sigmoid_d (const vector <float>& m1) {
+    
+    /*  Returns the value of the sigmoid function derivative f'(x) = f(x)(1 - f(x)),
+     where f(x) is sigmoid function.
+     Input: m1, a vector.
+     Output: x(1 - x) for every element of the input matrix m1.
+     */
+    
+    const unsigned long VECTOR_SIZE = m1.size();
+    vector <float> output (VECTOR_SIZE);
+    
+    
+    for( unsigned i = 0; i != VECTOR_SIZE; ++i ) {
+        output[ i ] = m1[ i ] * (1 - m1[ i ]);
+    }
+    
+    return output;
+}
+
+vector <float> sigmoid (const vector <float>& m1) {
+    
+    /*  Returns the value of the sigmoid function f(x) = 1/(1 + e^-x).
+     Input: m1, a vector.
+     Output: 1/(1 + e^-x) for every element of the input matrix m1.
+     */
+    
+    const unsigned long VECTOR_SIZE = m1.size();
+    vector <float> output (VECTOR_SIZE);
+    
+    
+    for( unsigned i = 0; i != VECTOR_SIZE; ++i ) {
+        output[ i ] = 1 / (1 + exp(-m1[ i ]));
+    }
+    
+    return output;
+}
+
+vector <float> operator+(const vector <float>& m1, const vector <float>& m2){
+    
+    /*  Returns the elementwise sum of two vectors.
+     Inputs:
+     m1: a vector
+     m2: a vector
+     Output: a vector, sum of the vectors m1 and m2.
+     */
+    
+    const unsigned long VECTOR_SIZE = m1.size();
+    vector <float> sum (VECTOR_SIZE);
+    
+    for (unsigned i = 0; i != VECTOR_SIZE; ++i){
+        sum[i] = m1[i] + m2[i];
+    };
+    
+    return sum;
+}
+
+vector <float> operator-(const vector <float>& m1, const vector <float>& m2){
+    
+    /*  Returns the difference between two vectors.
+     Inputs:
+     m1: vector
+     m2: vector
+     Output: vector, m1 - m2, difference between two vectors m1 and m2.
+     */
+    
+    const unsigned long VECTOR_SIZE = m1.size();
+    vector <float> difference (VECTOR_SIZE);
+    
+    for (unsigned i = 0; i != VECTOR_SIZE; ++i){
+        difference[i] = m1[i] - m2[i];
+    };
+    
+    return difference;
+}
+
+vector <float> operator*(const vector <float>& m1, const vector <float>& m2){
+    
+    /*  Returns the product of two vectors (elementwise multiplication).
+     Inputs:
+     m1: vector
+     m2: vector
+     Output: vector, m1 * m2, product of two vectors m1 and m2
+     */
+    
+    const unsigned long VECTOR_SIZE = m1.size();
+    vector <float> product (VECTOR_SIZE);
+    
+    for (unsigned i = 0; i != VECTOR_SIZE; ++i){
+        product[i] = m1[i] * m2[i];
+    };
+    
+    return product;
+}
+
+vector <float> operator*(const float m1, const vector <float>& m2){
+    
+    /*  Returns the product of a float and a vectors (elementwise multiplication).
+     Inputs:
+     m1: float
+     m2: vector
+     Output: vector, m1 * m2, product of two vectors m1 and m2
+     */
+    
+    const unsigned long VECTOR_SIZE = m2.size();
+    vector <float> product (VECTOR_SIZE);
+    
+    for (unsigned i = 0; i != VECTOR_SIZE; ++i){
+        product[i] = m1 * m2[i];
+    };
+    
+    return product;
+}
+
+vector <float> operator/(const vector <float>& m2, const float m1){
+    
+    /*  Returns the product of a float and a vectors (elementwise multiplication).
+     Inputs:
+     m1: float
+     m2: vector
+     Output: vector, m1 * m2, product of two vectors m1 and m2
+     */
+    
+    const unsigned long VECTOR_SIZE = m2.size();
+    vector <float> product (VECTOR_SIZE);
+    
+    for (unsigned i = 0; i != VECTOR_SIZE; ++i){
+        product[i] = m2[i] / m1;
+    };
+    
+    return product;
+}
+
+vector <float> transpose (float *m, const unsigned C, const unsigned R) {
+    
+    /*  Returns a transpose matrix of input matrix.
+     Inputs:
+     m: vector, input matrix
+     C: int, number of columns in the input matrix
+     R: int, number of rows in the input matrix
+     Output: vector, transpose matrix mT of input matrix m
+     */
+    
+    vector <float> mT (C*R);
+    
+    for(unsigned n = 0; n != C*R; n++) {
+        unsigned i = n/C;
+        unsigned j = n%C;
+        mT[n] = m[R*j + i];
+    }
+    
+    return mT;
+}
+
+vector <float> dot (const vector <float>& m1, const vector <float>& m2, const int m1_rows, const int m1_columns, const int m2_columns) {
+    
+    /*  Returns the product of two matrices: m1 x m2.
+     Inputs:
+     m1: vector, left matrix of size m1_rows x m1_columns
+     m2: vector, right matrix of size m1_columns x m2_columns (the number of rows in the right matrix
+     must be equal to the number of the columns in the left one)
+     m1_rows: int, number of rows in the left matrix m1
+     m1_columns: int, number of columns in the left matrix m1
+     m2_columns: int, number of columns in the right matrix m2
+     Output: vector, m1 * m2, product of two vectors m1 and m2, a matrix of size m1_rows x m2_columns
+     */
+    
+    vector <float> output (m1_rows*m2_columns);
+    
+    for( int row = 0; row != m1_rows; ++row ) {
+        for( int col = 0; col != m2_columns; ++col ) {
+            output[ row * m2_columns + col ] = 0.f;
+            for( int k = 0; k != m1_columns; ++k ) {
+                output[ row * m2_columns + col ] += m1[ row * m1_columns + k ] * m2[ k * m2_columns + col ];
+            }
+        }
+    }
+    
+    return output;
+}
+
+vector<string> split(const string &s, char delim) {
+    stringstream ss(s);
+    string item;
+    vector<string> tokens;
+    while (getline(ss, item, delim)) {
+        tokens.push_back(item);
+    }
+    return tokens;
+}
+
+int main(int argc, const char * argv[]) {
+
+    string line;
+    vector<string> line_v;
+
+    cout << "Loading data ...\n";
+    vector<float> X_train;
+    vector<float> y_train;
+    vector<vector<double>> w1_s;
+    vector<vector<double>> w2_s;
+    vector<vector<double>> w3_s;
+    ifstream myfile (DATAFOLDER + "/train.txt");
+    if (myfile.is_open())
+    {
+        while ( getline (myfile,line) )
+        {
+            line_v = split(line, '\t');
+            unsigned digit = strtof((line_v[0]).c_str(),0);
+            for (unsigned i = 0; i < 10; ++i) {
+                if (i == digit)
+                {
+                    y_train.push_back(1.);
+                }
+                else y_train.push_back(0.);
+            }
+            
+            unsigned size = static_cast<int>(line_v.size());
+            for (unsigned i = 1; i < size; ++i) {
+                X_train.push_back(strtof((line_v[i]).c_str(),0));
+            }
+        }
+        X_train = operator/(X_train, 255.0);
+        myfile.close();
+    }
+    
+    else cout << "Unable to open file" << '\n';
+    
+    // Some hyperparameters for the NN
+    unsigned BATCH_SIZE = 256;
+    float lr = .01/BATCH_SIZE;
+
+    // Random initialization of the weights
+    vector <float> W1 = random_vector(784*128);
+    vector <float> W2 = random_vector(128*64);
+    vector <float> W3 = random_vector(64*10);
+
+    cout << "Training the model ...\n";
+    for (unsigned i = 0; i < 10000; ++i) {
+
+        // Building batches of input variables (X) and labels (y)
+        unsigned randindx = rand() % (42000-BATCH_SIZE);
+        vector<float> b_X;
+        vector<float> b_y;
+        for (unsigned j = randindx*784; j < (randindx+BATCH_SIZE)*784; ++j){
+            b_X.push_back(X_train[j]);
+        }
+        for (unsigned k = randindx*10; k < (randindx+BATCH_SIZE)*10; ++k){
+            b_y.push_back(y_train[k]);
+        }
+
+        // Feed forward
+        vector<float> a1 = relu(dot( b_X, W1, BATCH_SIZE, 784, 128 ));
+        vector<float> a2 = relu(dot( a1, W2, BATCH_SIZE, 128, 64 ));
+        vector<float> yhat = softmax(dot( a2, W3, BATCH_SIZE, 64, 10 ), 10);
+        
+        // Back propagation
+        vector<float> dyhat = (yhat - b_y);
+        // dW3 = a2.T * dyhat
+        vector<float> dW3 = dot(transpose( &a2[0], BATCH_SIZE, 64 ), dyhat, 64, BATCH_SIZE, 10);
+        // dz2 = dyhat * W3.T * relu'(a2)
+        vector<float> dz2 = dot(dyhat, transpose( &W3[0], 64, 10 ), BATCH_SIZE, 10, 64) * reluPrime(a2);
+        // dW2 = a1.T * dz2
+        vector<float> dW2 = dot(transpose( &a1[0], BATCH_SIZE, 128 ), dz2, 128, BATCH_SIZE, 64);
+        // dz1 = dz2 * W2.T * relu'(a1)
+        vector<float> dz1 = dot(dz2, transpose( &W2[0], 128, 64 ), BATCH_SIZE, 64, 128) * reluPrime(a1);
+        // dW1 = X.T * dz1
+        vector<float> dW1 = dot(transpose( &b_X[0], BATCH_SIZE, 784 ), dz1, 784, BATCH_SIZE, 128);
+        
+        // Updating the parameters
+        W3 = W3 - lr * dW3;
+        W2 = W2 - lr * dW2;
+        W1 = W1 - lr * dW1;
+
+        
+        if ((i+1) % 100 == 0){
+            cout << "-----------------------------------------------Epoch " << i+1 << "--------------------------------------------------" <<"\n";
+            cout << "Predictions:" << "\n";
+            print ( yhat, 10, 10 );
+            cout << "Ground truth:" << "\n";
+            print ( b_y, 10, 10 );
+            vector<float> loss_m = yhat - b_y;
+            float loss = 0.0;
+            for (unsigned k = 0; k < BATCH_SIZE*10; ++k){
+                loss += loss_m[k]*loss_m[k];
+            }
+            cout << "                                            Loss " << loss/BATCH_SIZE <<"\n";
+            cout << "--------------------------------------------End of Epoch :(------------------------------------------------" <<"\n";
+        };
+
+        if(i==9999)
+		{
+			ofstream output1 (DATAFOLDER + "/w1.txt");
+            ofstream output2 (DATAFOLDER + "/w2.txt");
+            ofstream output3 (DATAFOLDER + "/w3.txt");
+			if(output1 && output2 && output3)
+			{	
+				for(unsigned i1=0; i1<784; i1++){
+                    vector<double> temp1;
+                    for(unsigned i2=0; i2<128; i2++){
+                        temp1.push_back(W1[i1+i2]);
+                    }
+                    w1_s.push_back(temp1);
+                }
+				for(unsigned i1=0; i1<128; i1++){
+                    vector<double> temp2;
+                    for(unsigned i2=0; i2<64; i2++){
+                        temp2.push_back(W2[i1+i2]);
+                    }
+                    w2_s.push_back(temp2);
+                }
+                for(unsigned i1=0; i1<64; i1++){
+                    vector<double> temp3;
+                    for(unsigned i2=0; i2<10; i2++){
+                        temp3.push_back(W3[i1+i2]);
+                    }
+                    w3_s.push_back(temp3);
+                }
+
+                unsigned size1 = static_cast<int>(w1_s.size());
+                unsigned size2 = static_cast<int>(w1_s[0].size());
+                for (unsigned i2 = 0; i2 < size2; ++i2) {
+                    for (unsigned i1 = 0; i1 < size1; ++i1) {
+                        output1 << w1_s[i1][i2] << "\t";
+                    }
+                    output1.seekp(-1 ,ios::cur);
+                    output1 << "\n";
+                }
+                unsigned size3 = static_cast<int>(w2_s.size());
+                unsigned size4 = static_cast<int>(w2_s[0].size());
+                for (unsigned i2 = 0; i2 < size4; ++i2) {
+                    for (unsigned i1 = 0; i1 < size3; ++i1) {
+                        output2 << w2_s[i1][i2] << "\t";
+                    }
+                    output2.seekp(-1 ,ios::cur);
+                    output2 << "\n";
+                }
+                unsigned size5 = static_cast<int>(w3_s.size());
+                unsigned size6 = static_cast<int>(w3_s[0].size());
+                for (unsigned i2 = 0; i2 < size6; ++i2) {
+                    for (unsigned i1 = 0; i1 < size5; ++i1) {
+                        output3 << w3_s[i1][i2] << "\t";
+                    }
+                    output3.seekp(-1 ,ios::cur);
+                    output3 << "\n";
+                }
+                
+				cout << "Model saved";
+			}
+			else 
+			{
+				cout << "Could not save model\n";
+			};
+		};
+    };
+    
+    return 0;
+}
