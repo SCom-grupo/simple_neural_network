5447af16663dc9bbcad247054fb08821600e73fe
xavixava <xavier.antunes@tecnico.ulisboa.pt>
2023-06-02 18:13:12 +0100

    updated downloader and created main.cpp. Also added comment template to use for the different versions

--- dataset/download_mnist.py
+++ dataset/download_mnist.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_openml
@@ -15,24 +17,23 @@ def fetch_classification_data(dataset="mnist_784", random_state=42):
     start_time = time.time()
     X, y = fetch_openml(dataset, version=1, return_X_y=True, as_frame=False)
     print("Downloaded data in {:.4f} seconds".format(time.time() - start_time))
-    # X /= 256  # normalize
     y = y.astype(int)  # fetch_openml loads it as a str
     print("Dataset has : " + str(len(X)))
-    # train_dev_X, train_dev_y = X[:60000], y[:60000]
     train_X, ev_X, train_y, ev_y = train_test_split(
-        X, y, train_size=60000, test_size=10000, random_state=random_state
+        X, y, train_size=60000, test_size=10000, random_state=int(random_state)
     )
-    
-    # These are commented for optimization purposes feel free to uncomment them
-    np.savetxt('train.txt', np.concatenate((train_y.reshape((60000, 1)), train_X), axis=1), fmt='%i', delimiter='\t')
-    np.savetxt('test.txt',np.concatenate((ev_y.reshape((10000, 1)), ev_X), axis=1), fmt='%i', delimiter='\t')
-    # np.savetxt('test_labels.txt', ev_y, fmt='%i', delimiter='\t')
-    np.savetxt('test_labels.txt', ev_y, fmt='%i', delimiter='\t')
-    # np.savetxt('test_features.txt', ev_X, fmt='%i', delimiter='\t')
-    np.savetxt('test_features.txt', np.transpose(ev_X), fmt='%i', delimiter='\t')
+    return (train_X, ev_X, train_y, ev_y )    
 
 
 if len(sys.argv) == 2:
-    fetch_classification_data(random_state=int(sys.argv[1]))
+    print(sys.argv)
+    train_X, ev_X, train_y, ev_y = fetch_classification_data(random_state=int(sys.argv[1]))
+    np.savetxt('../dataset/test.txt',np.concatenate((ev_y.reshape((10000, 1)), ev_X), axis=1), fmt='%i', delimiter='\t')
+    np.savetxt('../dataset/test_features.txt', np.transpose(ev_X), fmt='%i', delimiter='\t')
+    np.savetxt('../dataset/test_labels.txt', ev_y, fmt='%i', delimiter='\t')
 else:
-    fetch_classification_data()
+    train_X, ev_X, train_y, ev_y = fetch_classification_data()
+    np.savetxt('../dataset/train.txt',np.concatenate((train_y.reshape(60000, 1), train_X), axis=1), fmt='%i', delimiter='\t')
+    np.savetxt('../dataset/test.txt',np.concatenate((ev_y.reshape((10000, 1)), ev_X), axis=1), fmt='%i', delimiter='\t')
+    np.savetxt('../dataset/test_features.txt', np.transpose(ev_X), fmt='%i', delimiter='\t')
+    np.savetxt('../dataset/test_labels.txt', ev_y, fmt='%i', delimiter='\t')
---
+++ main.cpp
@@ -0,0 +1,151 @@
+/*
+ *
+ * This is meant to be the main source code for our project, after deleting 
+ * our tests sources
+ *
+ * */
+
+// Dependencies
+
+#include "openfhe.h"
+
+// header files needed for serialization
+#include "ciphertext-ser.h"
+#include "cryptocontext-ser.h"
+#include "key/key-ser.h"
+#include "scheme/ckksrns/ckksrns-ser.h"
+
+#define PROFILE
+using namespace lbcrypto;
+
+const std::string DATAFOLDER = "Data";
+const std::string DATASETFOLDER = "../dataset";
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <random>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+vector<string> split(const string &s, char delim);
+
+void accuracy_calc(const vector <double>&y, const vector< vector <double>>&y_hat, unsigned data_size);
+
+vector<vector<double>> readFileNormalized (string filename);
+
+int main(int argc, const char * argv[])
+{
+
+	// TODO: insert main flow here
+	
+	return 0;	
+
+}
+
+
+/*
+ * split
+ * args: s - the string which will be split in a string vector
+ * delim - the char separating each element in a string
+ *
+ * returns: a vector of strings
+ *
+ * description: splits a string by every delim char it finds
+ * Basically if delim == ',', it separates a csv line
+ * An helper function to read the dataset
+ *
+ * */
+
+vector<string> split(const string &s, char delim) {
+    stringstream ss(s);
+    string item;
+    vector<string> tokens;
+    while (getline(ss, item, delim)) {
+        tokens.push_back(item);
+    }
+    return tokens;
+}
+
+
+/*
+ * accuracy_calc 
+ * args: y - the correct labels
+ * y_hat - the results of the model computation on a dataset, should have shape (n_classes, dataset_size) 
+ * data_size - dataset size
+ *
+ * returns: nothing 
+ *
+ * description: compares computed results with the the given labels and prints the accuracy in percentage
+ * 
+ * */
+
+void accuracy_calc(const vector <double>&y, const vector< vector <double>>&y_hat, unsigned data_size){
+
+        unsigned BATCH_SIZE = data_size;
+        int counter=0, index=0;
+        float max=0.;
+        vector<float> max_save;
+
+	cout << "y hat has shape: (" << y_hat.size() << ", " << y_hat[0].size() << ")\n";
+
+        for (unsigned i=0; i<BATCH_SIZE; i++)
+        {
+                index=0;
+                max=0.;
+                for(unsigned j=0; j < 10; j++)
+                {
+                        if(y_hat[j][i]>max)
+                        {
+                                max = y_hat[j][i];
+                                index=j;
+                        };
+                };
+                if(y[i]==index) counter++;
+                max_save.push_back(index);
+        };
+        float count = counter;
+        cout << "       Accuracy = " << (count/BATCH_SIZE)*100 << "% " << counter << " right answers" << "\n";
+}
+
+/*
+ * readFile 
+ * args: filename - string with the path to the dataset_file 
+ *
+ * returns: a matrix(vector of vectors) with shape (dataset_size, n_features)
+ * the features will already scaled doubles between 0 and 1 
+ * 
+ * description: reads the data from file and returns it to perform computations on it 
+ * 
+ * */
+
+vector<vector<double>> readaFile(string filename){
+	ifstream input (DATASETFOLDER + filename);
+
+	vector<vector<double>> data;
+	if(input.is_open())
+	{
+		string line;
+		vector<string> line_v;
+		while (getline (input,line) )
+	 	{
+			line_v = split(line, '\t');
+			vector<double>  temp;
+			unsigned size = static_cast<int>(line_v.size());
+			for (unsigned i = 0; i < size; ++i) {
+				temp.push_back(strtof((line_v[i]).c_str(),0));
+			}
+			data.push_back(temp);
+		}
+	}
+	else cout << "Unable to open file" << '\n';
+	input.close();
+
+	return data;
+}
