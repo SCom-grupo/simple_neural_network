c663bc3d35fc0a605dcc0afc7283fceba6a5bcb4
NicoCastro30 <132573332+NicoCastro30@users.noreply.github.com>
2023-06-08 10:35:57 +0100

    Update main.cpp
    Main Code added with Switch

--- main.cpp
+++ main.cpp
@@ -43,7 +43,33 @@ vector<vector<double>> readFileNormalized (string filename);
 int main(int argc, const char * argv[])
 {
 
-	// TODO: insert main flow here
+	 if (argc < 2) {
+        std::cout << "No extra Command Line Argument passed" << std::endl;
+        return 1;
+    }
+
+    switch (argv[1][0]) {
+        case 'e':
+            std::cout << "Function Encrypt." << std::endl;
+            break;
+        case 'd':
+            std::cout << "Function Descrypt." << std::endl;
+            break;
+        case 'i':
+            std::cout << "Function Inference." << std::endl;
+            break;
+         case 't':
+            std::cout << "Function Training." << std::endl;
+            break;
+        default:
+            std::cout << "Don't exist this Function." << std::endl;
+            break;
+    }
+
+    for (int i = 0; i < argc; i++) {
+            cout << "argv[" << i << "]: " << argv[i]
+                 << '\n';
+        }
 	
 	return 0;	
 
